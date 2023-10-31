59dae5818145887dfa9518177b859cf246b9453f
bertauchekurtis <kurtisbertauche@gmail.com>
2023-10-31 16:26:35 -0700

    j

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+
+    Player player1 = new Player();
+    Player player2 = new Player();
+
+    player1.displayPieces();
+    player2.displayPieces();
+  }
+}
